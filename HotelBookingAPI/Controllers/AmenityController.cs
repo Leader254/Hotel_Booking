ea248e09df84698ca77ca86705c7e1a860d31579
Leader254 <gatheru.samuel@students.kyu.ac.ke>
2024-07-09 15:55:30 +0300

    added amenity controller

--- HotelBookingAPI/Controllers/AmenityController.cs
+++ HotelBookingAPI/Controllers/AmenityController.cs
@@ -38,5 +38,100 @@ namespace HotelBookingAPI.Controllers
                 return new APIResponse<AmenityFetchResultDTO>(HttpStatusCode.InternalServerError, "An error occurred while processing your request.", ex.Message);
             }
         }
+
+        [HttpGet("Fetch/{id}")]
+        public async Task<APIResponse<AmenityDetailsDTO>> FetchAmenityById(int amenityId)
+        {
+            try
+            {
+                var response = await _amenityRepository.FetchAmenityByIdAsync(amenityId);
+                if (response != null)
+                {
+                    return new APIResponse<AmenityDetailsDTO>(response, "Amenity Retreived Successfully");
+                }
+
+                return new APIResponse<AmenityDetailsDTO>(HttpStatusCode.NotFound, $"Amenity with Id ${amenityId}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occured while fetching amenity");
+                return new APIResponse<AmenityDetailsDTO>(HttpStatusCode.InternalServerError, "An error occured while processing your request", ex.Message);
+            }
+        }
+
+        [HttpPost("Add")]
+        public async Task<APIResponse<AmenityInsertResponseDTO>> AddAmenity([FromBody] AmenityInsertDTO payload)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return new APIResponse<AmenityInsertResponseDTO>(HttpStatusCode.BadRequest, "Invalid Data in the Request Body");
+                }
+                var response = await _amenityRepository.AddAmenityAsync(payload);
+                if (response.IsCreated)
+                {
+                    return new APIResponse<AmenityInsertResponseDTO>(response, response.Message);
+                }
+                return new APIResponse<AmenityInsertResponseDTO>(HttpStatusCode.BadRequest, response.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while adding amenity");
+                return new APIResponse<AmenityInsertResponseDTO>(HttpStatusCode.InternalServerError, "Amenity Creation Failed", ex.Message);
+            }
+        }
+
+        [HttpPut("Update/{id}")]
+        public async Task<APIResponse<AmenityUpdateResponseDTO>> UpdateAmenity(int id, [FromBody] AmenityUpdateDTO payload)
+        {
+            try
+            {
+                if (id != payload.AmenityID)
+                {
+                    _logger.LogInformation("Id didn't match");
+                    return new APIResponse<AmenityUpdateResponseDTO>(HttpStatusCode.BadRequest, "Mismatched Amenity ID");
+                }
+                if (!ModelState.IsValid)
+                {
+                    return new APIResponse<AmenityUpdateResponseDTO>(HttpStatusCode.BadRequest, "Invalid Request Body");
+                }
+
+                var response = await _amenityRepository.UpdateAmenityAsync(payload);
+                if (response.IsUpdated)
+                {
+                    return new APIResponse<AmenityUpdateResponseDTO>(response, response.Message);
+                }
+
+                return new APIResponse<AmenityUpdateResponseDTO>(HttpStatusCode.BadRequest, response.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while updating amenity");
+                return new APIResponse<AmenityUpdateResponseDTO>(HttpStatusCode.InternalServerError, "Error occurred while processing your request", ex.Message);
+            }
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public async Task<APIResponse<AmenityDeleteResponseDTO>> DeleteAmenity(int id)
+        {
+            try
+            {
+                var amenity = await _amenityRepository.FetchAmenityByIdAsync(id);
+                if(amenity == null){
+                    return new APIResponse<AmenityDeleteResponseDTO>(HttpStatusCode.NotFound, $"Amenity with Id{id} is not found");
+                }
+                var response = await _amenityRepository.DeleteAmenityAsync(id);
+                if(response.IsDeleted){
+                    return new APIResponse<AmenityDeleteResponseDTO>(response, response.Message);
+                }
+                return new APIResponse<AmenityDeleteResponseDTO>(HttpStatusCode.BadRequest, response.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while deleting amenity.");
+                return new APIResponse<AmenityDeleteResponseDTO>(HttpStatusCode.InternalServerError, "Error occurred while trying to process your request", ex.Message);
+            }
+        }
     }
 }
